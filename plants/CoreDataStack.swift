74ec3248b2a6a1a9d753d0e26211f1e85b1d936e
bsimps01 <b.albertsimpson@gmail.com>
2020-09-15 15:41:55 -0700

    first commit

---
+++ plants/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  plants
+//
+//  Created by Benjamin Simpson on 9/10/20.
+//  Copyright © 2020 Adriana González Martínez. All rights reserved.
+//
+
+import Foundation
